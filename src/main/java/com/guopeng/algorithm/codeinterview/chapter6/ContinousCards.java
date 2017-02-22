b36821464c642410a7d53d6dad8da94511ee29f3
guopeng <532957426@qq.com>
2017-02-22 17:36:50 +0800

    debug

---
+++ src/main/java/com/guopeng/algorithm/codeinterview/chapter6/ContinousCards.java
@@ -0,0 +1,10 @@
+package com.guopeng.algorithm.codeinterview.chapter6;
+
+/**
+ * Created by guopeng on 17-2-22.
+ */
+public class ContinousCards {
+    public boolean isContinue(int[] arr) {
+
+    }
+}
