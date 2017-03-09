e9f4795a2df8e68d0257733e7281076a371b982b
Peterkwok <532957426@qq.com>
2017-03-09 08:23:00 +0800

    BM

---
+++ src/main/java/com/guopeng/algorithm/util/BM.java
@@ -0,0 +1,30 @@
+package com.guopeng.algorithm.util;
+
+/**
+ * Created by guopeng on 2017/3/9.
+ */
+public class BM {
+    public int search(String pattern, String str) {
+        int[] right = skipArr(pattern);
+
+        for (int i = 0, step = 0; i < str.length(); i += step) {
+            for (int j = pattern.length() - 1; j >= 0; j--)
+                if (str.charAt(i + i) != pattern.charAt(j)) {
+                    step = j - right[str.charAt(i + j)];
+                    if (step < 1) step = 1;
+                    break;
+                }
+            if (step == 0) return i;
+        }
+        return -1;
+    }
+
+    private int[] skipArr(String pattern) {
+        int[] right = new int[256];
+        for (int i = 0; i < 256; i++)
+            right[i] = -1;
+        for (int i = 0; i < pattern.length(); i++)
+            right[pattern.charAt(i)] = i;
+        return right;
+    }
+}
