01ccec95ff27779b3c190146c6c3a6b52e32c2f2
guopeng <532957426@qq.com>
2017-03-20 21:32:34 +0800

    Tuple

---
+++ src/main/java/com/guopeng/algorithm/util/Tuple.java
@@ -0,0 +1,33 @@
+package com.guopeng.algorithm.util;
+
+/**
+ * Created by guopeng on 17-3-20.
+ */
+public class Tuple {
+    public int a;
+    public int b;
+
+    public Tuple(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public int hashCode() {
+        return a * 31 + b;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Tuple tuple = (Tuple) obj;
+        return a == tuple.a && b == tuple.b;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(a=%d,b=%d)", a, b);
+    }
+}
