6cb491c2c24abc0c374b4696facf234ad9e37167
guopeng <532957426@qq.com>
2017-03-20 16:08:08 +0800

    CutRod

---
+++ src/main/java/com/guopeng/algorithm/real/leetcode/DynamicProgramming/CutRod.java
@@ -0,0 +1,43 @@
+package com.guopeng.algorithm.real.leetcode.DynamicProgramming;
+
+import com.guopeng.algorithm.codeinterview.utils.Print;
+
+/**
+ * Created by guopeng on 17-3-20.
+ */
+public class CutRod {
+    private static int[] maxPrice;
+    private static int[] leftLength;
+
+    public int cutRod(int n, int[] price) {
+        if (maxPrice == null) {
+            maxPrice = new int[n + 1];
+            leftLength = new int[n + 1];
+        }
+
+        if (n <= 0) return 0;
+        int p = 0;
+        for (int i = 1; i < Math.min(price.length, n + 1); i++) {
+            p = Math.max(p, price[i] + cutRod(n - i, price));
+            if (p > maxPrice[n]) {
+                maxPrice[n] = p;
+                leftLength[n] = i;
+            }
+        }
+        return p;
+    }
+
+    public String findCut(int n, String cut) {
+        if (n == 0) return cut.substring(0, cut.length() - 1);
+        return findCut(n - leftLength[n], cut + leftLength[n] + ",");
+    }
+
+    public static void main(String[] args) {
+        int[] price = {0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+        CutRod cut = new CutRod();
+        cut.cutRod(10, price);
+        Print.arrPrint(maxPrice);
+        Print.arrPrint(leftLength);
+        System.out.println(cut.findCut(5, ""));
+    }
+}
