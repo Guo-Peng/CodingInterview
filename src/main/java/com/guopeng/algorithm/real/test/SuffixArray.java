93a97ebad7874cca378a3069b2a4beebba787a2d
guopeng <532957426@qq.com>
2017-04-06 11:57:47 +0800

    getRank

--- src/main/java/com/guopeng/algorithm/real/test/SuffixArray.java
+++ src/main/java/com/guopeng/algorithm/real/test/SuffixArray.java
@@ -6,50 +6,56 @@ import com.guopeng.algorithm.codeinterview.utils.Print;
  * Created by guopeng on 2017/4/3.
  */
 public class SuffixArray {
-    public static int[] getRank(String str) {
+    public static int[] suffixArray(String str) {
         int n = str.length();
         int[] rank = initRank(str);
-        int[] secRank = new int[2 * n];
+        int[] secSa = new int[2 * n];
         int[] tmp;
         int[] sa = new int[n];
         int[] count = new int[n];
 
-        for (int i = 0; i < n; i++)
+        int i, l, p, max = 0;
+
+        for (i = n; i < 2 * n; i++) {
+            rank[i] = -1;
+            secSa[i] = -1;
+        }
+
+        for (i = 0; i < n; i++) {
             count[rank[i]]++;
-        for (int i = 1; i < n; i++)
+            if (rank[i] > max) max = rank[i] + 1;
+        }
+
+        for (i = 1; i < max; i++)
             count[i] += count[i - 1];
-        for (int i = n - 1; i >= 0; i--)
+        for (i = n - 1; i >= 0; i--)
             sa[--count[rank[i]]] = i;
 
-        int i, l, p;
-        for (l = 1, p = 1; p < n; l *= 2) {
+        for (l = 1, p = 1; p < n; l *= 2, max = p) {
             for (i = n - l, p = 0; i < n; i++)
-                secRank[p++] = i;
+                secSa[p++] = i;
 
             for (i = 0; i < n; i++)
                 if (sa[i] >= l)
-                    secRank[p++] = sa[i] - l;
+                    secSa[p++] = sa[i] - l;
 
-            for (i = 0; i < n; i++)
+            for (i = 0; i < max; i++)
                 count[i] = 0;
             for (i = 0; i < n; i++)
-                count[rank[secRank[i]]]++;
-            for (i = 1; i < n; i++)
+                count[rank[secSa[i]]]++;
+            for (i = 1; i < max; i++)
                 count[i] += count[i - 1];
             for (i = n - 1; i >= 0; i--)
-                sa[--count[rank[secRank[i]]]] = secRank[i];
+                sa[--count[rank[secSa[i]]]] = secSa[i];
 
-            for (tmp = rank, rank = secRank, secRank = tmp, p = 1, rank[sa[0]] = 0, i = 1; i < n; i++)
-                rank[sa[i]] = cmp(secRank, sa[i - 1], sa[i], l) ? p - 1 : p++;
-
-            Print.arrPrint(rank);
-            System.out.println(l);
+            for (tmp = rank, rank = secSa, secSa = tmp, p = 1, rank[sa[0]] = 0, i = 1; i < n; i++)
+                rank[sa[i]] = cmp(secSa, sa[i - 1], sa[i], l) ? p - 1 : p++;
         }
 
         return sa;
     }
 
-    public static boolean cmp(int[] r, int a, int b, int l) {
+    private static boolean cmp(int[] r, int a, int b, int l) {
         return r[a] == r[b] && r[a + l] == r[b + l];
     }
 
@@ -66,10 +72,22 @@ public class SuffixArray {
         return rank;
     }
 
+    private static int[] getRank(int[] sa) {
+        int[] rank = new int[sa.length];
+        for (int i = 0; i < sa.length; i++) {
+            rank[sa[i]] = i;
+        }
+        return rank;
+    }
+
 
     public static void main(String[] args) {
-        String str = "aabaaaab";
-//        System.out.println((char) 94);
-        Print.arrPrint(getRank(str));
+        String str = "abcdefg";
+        int[] sa = suffixArray(str);
+        int[] rank = getRank(sa);
+        System.out.println("sa 数组：");
+        Print.arrPrint(sa);
+        System.out.println("rank 数组：");
+        Print.arrPrint(rank);
     }
 }
