51ae840fbaa36c48cdff831d38d7e9f807bdde15
Peterkwok <532957426@qq.com>
2017-04-08 13:39:26 +0800

    largestRectangleArea

--- src/main/java/com/guopeng/algorithm/real/leetcode/LargestRectangleArea.java
+++ src/main/java/com/guopeng/algorithm/real/leetcode/LargestRectangleArea.java
@@ -63,11 +63,37 @@ public class LargestRectangleArea {
         return max;
     }
 
+    public int largestRectangleArea3(int[] heights) {
+        int n = heights.length;
+
+        Stack<Integer> index = new Stack<>();
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            if (!(index.empty() || heights[i] >= heights[index.peek()])) {
+                while (!index.empty() && heights[i] < heights[index.peek()]) {
+                    int top = index.pop();
+                    int area = (index.empty() ? i : (i - index.peek() - 1)) * heights[top];
+                    if (area > max) max = area;
+                }
+            }
+            index.push(i);
+        }
+
+        while (!index.empty()) {
+
+            int top = index.pop();
+            int area = (index.empty() ? n : (n - index.peek() - 1)) * heights[top];
+            if (area > max) max = area;
+        }
+        return max;
+    }
+
     /**
      * 只入栈递增序列，索引为i时出现递减则将所有高度大于h[i]的出栈
      * 出栈部分的序列中的立柱向两侧扩展的范围仅限于序列之中，之外的立柱都比他们低的所隔开
      * 因此出栈的序列即是求解最大矩形的孤岛，它们不会干扰到外部
-     * 出栈的部分的最大面积即为(i-k)*h[k],k为出栈的索引
+     * 出栈的部分的最大面积即为(i-k_before )*h[k],k为出栈的索引
+     * 矩形宽度使用当前所以i与栈顶的索引得到，因为出栈元素与栈顶元素之间可能还有其他元素并且已经出栈，出栈代表它们比当前出栈元素更小，宽度要算入其中
      * 栈为空则代表当前的栈的第一个立柱要低于0 - i-1的所有立柱，因此其面积为i*h[k]
      *
      * @param heights
@@ -78,12 +104,13 @@ public class LargestRectangleArea {
         Stack<Integer> s = new Stack<>();
         int maxArea = 0;
         for (int i = 0; i <= len; i++) {
-            int h = (i == len ? 0 : heights[i]);
+            int h = (i == len ? 0 : heights[i]); // 数组末尾添加0保证最后全部出栈进行计算，之前有0并不影响
             if (s.isEmpty() || h >= heights[s.peek()]) {
                 s.push(i);
             } else {
                 int tp = s.pop();
                 // 注意栈为空的情况
+                // 注意矩形宽度的计算，出栈元素与栈顶之间可能还有其他元素，这些元素都大于当前出栈元素
                 maxArea = Math.max(maxArea, heights[tp] * (s.isEmpty() ? i : i - 1 - s.peek()));
                 i--; // 保证i不变直到出栈顶的立柱低于当前立柱
             }
