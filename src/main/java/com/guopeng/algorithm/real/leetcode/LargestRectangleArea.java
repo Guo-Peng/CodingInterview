f6bc5c2d753cce37dfcce26451bb71deade47a5a
Peterkwok <532957426@qq.com>
2017-04-08 12:36:28 +0800

    largestRectangleArea2

--- src/main/java/com/guopeng/algorithm/real/leetcode/LargestRectangleArea.java
+++ src/main/java/com/guopeng/algorithm/real/leetcode/LargestRectangleArea.java
@@ -10,6 +10,11 @@ import java.util.Stack;
 public class LargestRectangleArea {
     /**
      * 计算子数组的最大面积，不断扩张
+     * 先计算一个立柱的面积
+     * 然后计算以i为起点的两个立柱面积
+     * 依次类推至长度为n-1
+     * 复杂度o(n^2)
+     * 超时
      *
      * @param heights
      * @return
@@ -32,6 +37,29 @@ public class LargestRectangleArea {
             }
         }
 
+        return max;
+    }
+
+    /**
+     * 矩形必然以某一立柱的高度为边
+     * 遍历矩形并将其左右扩展,左右高度必须大于等于当前立柱高度
+     * 复杂度o(n^2)
+     * 超时
+     *
+     * @param heights
+     * @return
+     */
+    public int largestRectangleArea2(int[] heights) {
+        int n = heights.length;
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            int count = 1;
+            for (int l = i - 1; l >= 0 && heights[l] >= heights[i]; l--)
+                count++;
+            for (int r = i + 1; r < n && heights[r] >= heights[i]; r++)
+                count++;
+            if (heights[i] * count > max) max = heights[i] * count;
+        }
         return max;
     }
 
