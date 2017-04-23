245017d4d04c6ed4a35e19bf843589b550c39ae9
Peterkwok <532957426@qq.com>
2017-04-23 16:21:12 +0800

    3 sum

--- src/main/java/com/guopeng/algorithm/real/code/Math/ThreeSum.java
+++ src/main/java/com/guopeng/algorithm/real/code/Math/ThreeSum.java
@@ -43,22 +43,42 @@ public class ThreeSum {
                 if (nums[i] + nums[j] + nums[k] == 0) {
                     result.add(Arrays.asList(nums[i], nums[j], nums[k]));
 
-                    do {
-                        j++;
-                    } while (j < k && nums[j] == nums[j - 1]);
+                    while (++j < k && nums[j] == nums[j - 1]) ;
+                    while (--k > j && nums[k] == nums[k + 1]) ;
+                } else if (nums[i] + nums[j] + nums[k] < 0)
+                    while (++j < k && nums[j] == nums[j - 1]) ;
+                else
+                    while (--k > j && nums[k] == nums[k + 1]) ;
 
-                    do {
-                        k--;
-                    } while (k > j && nums[k] == nums[k + 1]);
-                } else if (nums[i] + nums[j] + nums[k] < 0) {
-                    do {
-                        j++;
-                    } while (j < k && nums[j] == nums[j - 1]);
-                } else {
-                    do {
-                        k--;
-                    } while (k > j && nums[k] == nums[k + 1]);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int threeSumClosest(int[] nums, int target) {
+        int n = nums.length;
+        if (n < 3) return 0;
+
+        Arrays.sort(nums);
+        int result = nums[0] + nums[1] + nums[2];
+        int min = Math.abs(target - (nums[0] + nums[1] + nums[2]));
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1, k = n - 1; j < k; ) {
+                if (Math.abs(target - (nums[i] + nums[j] + nums[k])) < min) {
+                    min = Math.abs(target - (nums[i] + nums[j] + nums[k]));
+                    result = nums[i] + nums[j] + nums[k];
                 }
+                if (target - (nums[i] + nums[j] + nums[k]) == 0)
+                    return target;
+                else if (target - (nums[i] + nums[j] + nums[k]) > 0)
+                    j++;
+                else
+                    k--;
             }
         }
         return result;
@@ -73,6 +93,9 @@ public class ThreeSum {
 
         arr = new int[]{-2, 0, 0, 2, 2};
         System.out.println(threeSum(arr));
+
+        arr = new int[]{-1, 2, 1, -4};
+        System.out.println(threeSumClosest(arr, 1));
 
     }
 }
