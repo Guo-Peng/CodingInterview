d1a3984d503c511f86bf72bd587d7460e369689f
guopeng <532957426@qq.com>
2017-04-28 15:37:01 +0800

    Interval

---
+++ src/main/java/com/guopeng/algorithm/real/code/Array/Interval.java
@@ -0,0 +1,19 @@
+package com.guopeng.algorithm.real.code.Array;
+
+/**
+ * Created by guopeng on 17-4-28.
+ */
+public class Interval {
+    int start;
+    int end;
+
+    public Interval() {
+        start = 0;
+        end = 0;
+    }
+
+    public Interval(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+}
