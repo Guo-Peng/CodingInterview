634489e31bb7e927f974d23ed635e316638e58d5
guopeng <532957426@qq.com>
2017-05-03 10:23:46 +0800

    FindMinimumInRotatedSortedArray

---
+++ src/main/java/com/guopeng/algorithm/real/code/Array/FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,32 @@
+package com.guopeng.algorithm.real.code.Array;
+
+/**
+ * Created by guopeng on 17-5-3.
+ */
+public class FindMinimumInRotatedSortedArray {
+    public static int findMin(int[] nums) {
+        int n = nums.length;
+
+        return find(nums, 0, n - 1);
+    }
+
+    private static int find(int[] nums, int low, int hi) {
+        if (low >= hi) return nums[low];
+        int mid = low + (hi - low) / 2;
+
+        if (mid > 0 && nums[mid] < nums[mid - 1])
+            return nums[mid];
+
+        if (nums[mid] >= nums[low] && nums[mid] > nums[hi])
+            return find(nums, mid + 1, hi);
+        else return find(nums, low, mid - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        System.out.println(findMin(arr));
+
+        arr = new int[]{3, 1, 2};
+        System.out.println(findMin(arr));
+    }
+}
