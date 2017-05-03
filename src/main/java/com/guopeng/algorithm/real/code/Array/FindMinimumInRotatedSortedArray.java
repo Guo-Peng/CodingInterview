934b7099ef521d0f9cb41a9b7b834050d22665f0
guopeng <532957426@qq.com>
2017-05-03 11:09:48 +0800

    FindMinimumInRotatedSortedArray

--- src/main/java/com/guopeng/algorithm/real/code/Array/FindMinimumInRotatedSortedArray.java
+++ src/main/java/com/guopeng/algorithm/real/code/Array/FindMinimumInRotatedSortedArray.java
@@ -8,6 +8,7 @@ public class FindMinimumInRotatedSortedArray {
      * 元素大于开头及结尾则一定在右侧
      * 否则在左侧
      * 如果元素小于其前一个则该元素就是最小
+     *
      * @param nums
      * @return
      */
@@ -27,6 +28,41 @@ public class FindMinimumInRotatedSortedArray {
         if (nums[mid] >= nums[low] && nums[mid] > nums[hi])
             return find(nums, mid + 1, hi);
         else return find(nums, low, mid - 1);
+    }
+
+    public static int findMinII(int[] nums) {
+        int n = nums.length;
+
+        return findII(nums, 0, n - 1);
+    }
+
+    private static int findII(int[] nums, int low, int hi) {
+        if (low == hi) return nums[low];
+
+        if (nums[low] < nums[hi]) return nums[low];
+
+        int mid = low + (hi - low) / 2;
+        if (nums[low] == nums[mid] && nums[hi] == nums[mid])
+            return Math.min(findII(nums, low, mid - 1), findII(nums, mid + 1, hi));
+
+        if (nums[mid] >= nums[low]) {
+            return Math.min(nums[mid], findII(nums, mid + 1, hi));
+        } else return Math.min(nums[mid], findII(nums, low, mid - 1));
+    }
+
+    public static int findMinIII(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] < nums[r]) {
+                r = mid;
+            } else if (nums[mid] > nums[r]) {
+                l = mid + 1;
+            } else {
+                r--;  //nums[mid]=nums[r] no idea, but we can eliminate nums[r];
+            }
+        }
+        return nums[l];
     }
 
     public static void main(String[] args) {
