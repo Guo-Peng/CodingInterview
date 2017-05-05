525410c86253386444717c56f0188064d4caa9a2
guopeng <532957426@qq.com>
2017-05-05 10:26:46 +0800

    MedianFinder

---
+++ src/main/java/com/guopeng/algorithm/real/code/Heap/MedianFinder.java
@@ -0,0 +1,65 @@
+package com.guopeng.algorithm.real.code.Heap;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * Created by guopeng on 17-5-5.
+ */
+public class MedianFinder {
+    PriorityQueue<Integer> left;
+    PriorityQueue<Integer> right;
+
+    class com implements Comparator<Integer> {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return o2 - o1;
+        }
+    }
+
+    /**
+     * initialize your data structure here.
+     */
+    public MedianFinder() {
+        left = new PriorityQueue<>(new com());
+        right = new PriorityQueue<>();
+    }
+
+    public void addNum(int num) {
+        left.add(num);
+        right.add(left.remove());
+        if (left.size() < right.size())
+            left.add(right.remove());
+    }
+
+    public double findMedian() {
+        if (left.size() > right.size())
+            return left.peek();
+        else return ((double) left.peek() + right.peek()) / 2;
+    }
+
+    public static void main(String[] args) {
+        MedianFinder obj = new MedianFinder();
+
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        for (int num : arr) {
+            obj.addNum(num);
+//            System.out.println(obj.findMedian());
+        }
+
+        obj = new MedianFinder();
+
+        arr = new int[]{6, 10, 2, 6, 5, 0, 6};
+        for (int num : arr) {
+            obj.addNum(num);
+        }
+        System.out.println(obj.findMedian());
+    }
+
+}
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
