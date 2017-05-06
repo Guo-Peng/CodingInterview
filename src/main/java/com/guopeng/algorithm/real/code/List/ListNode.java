f324862367a2fc13a13922d59a85e4ba06a170cf
Peterkwok <532957426@qq.com>
2017-05-06 23:04:10 +0800

    ListNode

--- src/main/java/com/guopeng/algorithm/real/code/List/ListNode.java
+++ src/main/java/com/guopeng/algorithm/real/code/List/ListNode.java
@@ -4,10 +4,10 @@ package com.guopeng.algorithm.real.code.List;
  * Created by guopeng on 17-4-28.
  */
 public class ListNode {
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
 
-    ListNode(int x) {
+    public ListNode(int x) {
         val = x;
     }
 }
