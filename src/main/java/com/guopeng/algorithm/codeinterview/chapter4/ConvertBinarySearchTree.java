0bf0eb97535d0e17e3f4dc03567246664cba8a93
Peterkwok <532957426@qq.com>
2017-05-07 16:54:48 +0800

    ConvertBinarySearchTree

--- src/main/java/com/guopeng/algorithm/codeinterview/chapter4/ConvertBinarySearchTree.java
+++ src/main/java/com/guopeng/algorithm/codeinterview/chapter4/ConvertBinarySearchTree.java
@@ -27,19 +27,14 @@ public class ConvertBinarySearchTree {
     }
 
     private BinaryTreeNode convertSubTree(BinaryTreeNode root, BinaryTreeNode lastNode) {
-        if (root == null) return null;
-        if (root.left != null)  //左叶子节点为当前子树的最小节点，将其与前last连接
-            lastNode = convertSubTree(root.left, lastNode); // 得到左子树转化后的最后一个节点
+        if (root == null) return lastNode;//左叶子节点为当前子树的最小节点，将其与前last连接
+        lastNode = convertSubTree(root.left, lastNode); // 得到左子树转化后的最后一个节点
 
         root.left = lastNode;
-        if (lastNode != null)
-            lastNode.right = root;
+        if (lastNode != null) lastNode.right = root;
 
         lastNode = root;
-        if (root.right != null)
-            lastNode = convertSubTree(root.right, lastNode);
-
-        return lastNode;
+        return convertSubTree(root.right, lastNode);
     }
 
     public String printList(BinaryTreeNode root) {
