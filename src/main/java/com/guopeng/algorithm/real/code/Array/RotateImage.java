707b29ea3cd185aec2c480b67bb5a37c27898ac1
guopeng <532957426@qq.com>
2017-05-10 17:00:43 +0800

    RotateImage

---
+++ src/main/java/com/guopeng/algorithm/real/code/Array/RotateImage.java
@@ -0,0 +1,26 @@
+package com.guopeng.algorithm.real.code.Array;
+
+import com.guopeng.algorithm.codeinterview.utils.Print;
+
+/**
+ * Created by guopeng on 17-5-10.
+ */
+public class RotateImage {
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = i; j < n - 1 - i; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = tmp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{0, 1, 2, 3}, {4, 5, 6, 7}, {8, 9, 10, 11}, {12, 13, 14, 15}};
+        rotate(matrix);
+    }
+}
